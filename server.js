3cf6d7fecb3c65fc0dcdb10b1e73f298f2aff6d3
rowgregory <rowgregory@gmail.com>
2019-07-16 19:43:44 -0400

    add heroku cli

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -9,15 +10,25 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactKeeper API" })
-);
-
 // Define Routes
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // checking the environment, if its in production load
+  // a static folder, react build folder, and then have
+  // this route so when its hit the home page it is going
+  // to load the index.html inside the client build folder
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
@@ -31,5 +42,3 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 // e-commerce
 // portland, ME
 // MERN stack
-
-// This is a test
